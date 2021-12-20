808563a35b1b7ce6adf3ed46d8638ded2d6bf661
Roc-ny <93367204+Roc-ny@users.noreply.github.com>
2021-12-20 16:16:00 +0800

    first commit

---
+++ src/main/java/com/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+/**
+ * @author Roc
+ * @version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,World");
+    }
+}
